2ad386caee3f33f839f8a467fa0c3a1d06be6530
projecTym <tym.jones92@gmail.com>
2021-01-08 18:24:44 -0600

    fixed useFetch from infinite rerender

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,26 +1,38 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 export const useFetch = () => {
 
+    const [requestMade, setRequestMade] = useState(false)
     const [loading, setLoading] = useState(true);
     const [songs, setSongs] = useState([]);
     const [keys, setKeys] = useState([]);
 
 
     const getSongs = async () => {
-        const response = await fetch('https://my-json-server.typicode.com/projectym/Songs-Api/db');
-        const data = await response.json();
-        console.log(data)
 
-        setSongs(data.Songs);
-        setKeys(data.Keys);
-        setLoading(false);
-        console.log(loading, songs, keys)
+        setRequestMade(true)
+        console.log("trying to recieve data")
+
+        try {
+            const response = await fetch('https://my-json-server.typicode.com/projectym/Songs-Api/db')
+            const data = await response.json()
+
+            setSongs(data.Songs);
+            setKeys(data.Keys);
+            console.log(loading, songs, keys)
+            setLoading(false);
+
+        }
+        catch (err) {
+            console.log(err)
+        }
+
     }
 
-    useEffect(() => {
+    if (!requestMade) {
         getSongs()
-    })
+    }
+
 
     return { loading, songs, keys }
 
